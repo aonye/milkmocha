598812123e90addd76accf2542e0f531881305a9
andy.ye <andy.ye@Andys-MacBook-Pro-2.local>
2024-03-15 15:57:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import milkBounce from "./milk_bounce.webp";
+import bubuDudu from "./bubu-dudu-kiss.gif";
 
-function App() {
+import "./App.scss";
+
+const App = () => {
+  const [clickedYes, setClickedYes] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [size, setSize] = useState({
+    width: 100,
+    height: 50,
+    fontSize: 16,
+  });
+
+  const arrayOfRejections = [
+    "No",
+    "Are you sure?",
+    "Really sure?",
+    "Think again",
+    "Last chance!",
+    "You might regret this",
+    "Give it another thought",
+    "Are you absolutely certain?",
+    "This could be a mistake!",
+    "Have a heart!",
+    "Don't be so cold",
+    "Change of heart?",
+    "Wouldn't you reconsider?",
+    "Is that your final answer?",
+    "You've breaking my heart :'(",
+  ];
+
+  const resizeHandler = () => {};
+
+  const onRedClickHandler = () => {
+    if (!(index >= arrayOfRejections.length - 1)) {
+      setSize((prev) => Object.fromEntries(
+        Object.entries(prev).map(([key, value]) => [key, value * 1.2])));
+      setIndex((prev) => prev + 1);
+    }
+  };
+
+  const onGreenClickHandler = () => {
+    setClickedYes(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {clickedYes ? (
+        <>
+          <header>Ok yayyy!!!</header>
+          <img src={bubuDudu} alt="bubududu"></img>
+        </>
+      ) : (
+        <>
+          <header>Be my valentine?</header>
+          <img src={milkBounce} alt="milk mocha" />
+          <div className="buttons-container">
+            <button
+              className="button button-green"
+              style={{
+                width: size.width,
+                height: size.height,
+                fontSize: size.fontSize,
+              }}
+              onClick={() => onGreenClickHandler()}
+            >
+              Yes
+            </button>
+            <button className="button button-red" onClick={() => onRedClickHandler()}>{arrayOfRejections[index]}</button>
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
